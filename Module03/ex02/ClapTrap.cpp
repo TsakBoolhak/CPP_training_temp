f6ebdfc30469be5b8aefecd2bdf3ce0fee039aa1
Aurelien CABIAC <acabiac@e2r3p16.clusters.42paris.fr>
2022-02-16 17:19:47 +0100

    Module03 In Progress

---
+++ Module03/ex02/ClapTrap.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <limits>
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap( void ) : _name ( "Default name" ), _hitPoints ( 10 ), _energyPoints ( 10 ), _attackDamage ( 0 ) {
+
+	std::cout << "Default ClapTrap constructor called" << std::endl;
+
+	return ;
+}
+
+ClapTrap::ClapTrap( ClapTrap const & src ) {
+
+	std::cout << "Copy ClapTrap constructor called" << std::endl;
+
+	*this = src;
+
+	return ;
+}
+
+ClapTrap::ClapTrap( std::string name ) : _name ( name ), _hitPoints ( 10 ), _energyPoints ( 10 ), _attackDamage ( 0 ) {
+
+	std::cout << "String ClapTrap constructor called" << std::endl;
+
+	return ;
+}
+
+ClapTrap::~ClapTrap( void ) {
+
+	std::cout << "Claptrap destructor called" << std::endl;
+
+	return ;
+}
+
+void	ClapTrap::attack( const std::string& target ) {
+
+	std::cout << this->_name;
+
+	if (this->_hitPoints == 0)
+	std::cout << " is lying on the ground, uncouncious and is unable to attack " << target << "!" << std::endl;
+
+	else if ( this->_energyPoints > 0) {
+
+		this->_energyPoints--;
+		std::cout << " attacks " << target << ", causing " << this->_attackDamage << " points of damage ! He now has " << this->_energyPoints << " energy points left!"<< std::endl;
+	}
+
+	else
+		std::cout << " is too exhausted to attack " << target << " and just mumble under his breath..." << std::endl;
+
+	return ;
+}
+
+void	ClapTrap::takeDamage( unsigned int amount ) {
+
+	std::cout << this->_name;
+
+	if ( this->_hitPoints == 0 )
+		std::cout << " is already lying on the ground uncouncious and can't take any more damage!" << std::endl;
+
+	else if ( amount >= this->_hitPoints) {
+
+		std::cout << " has taken " << this->_hitPoints << " damage";
+		if( amount > this->_hitPoints )
+			std::cout << " out of the " << amount << " potential damage of the attack";
+		std::cout << " and falls uncouncious on the ground as he has 0 hitpoints left!" << std::endl;
+		this->_hitPoints = 0;
+	}
+
+	else {
+
+		this->_hitPoints -= amount;
+		std::cout << " has taken " << amount << " damage, he now has " << this->_hitPoints << " hitpoints left!" << std::endl;
+	}
+
+	return ;
+}
+
+void	ClapTrap::beRepaired( unsigned int amount ) {
+
+	std::cout << this->_name;
+
+	if ( this->_hitPoints == 0 )
+		std::cout << " would have try to repair himself if he wasn't uncouncious!" << std::endl;
+
+	else if (this->_energyPoints == 0)
+		std::cout << " is trying to repair himself but feels too tired to activate this protocol!" << std::endl; 
+
+	else {
+
+		this->_hitPoints = std::numeric_limits<unsigned int>::max() - amount < this->_hitPoints ? std::numeric_limits<unsigned int>::max() : this->_hitPoints + amount;
+		this->_energyPoints--;
+
+		std::cout << " has activated the repair protocol with a voltage of " << amount << "V he now has " << this->_hitPoints << " hit points and " << this->_energyPoints << " energy points left!" << std::endl;
+	}
+
+	return ;
+}
+
+ClapTrap &	ClapTrap::operator=( ClapTrap const & rhs ) {
+
+	if (this != &rhs) {
+
+		this->_hitPoints = rhs._hitPoints;
+		this->_energyPoints = rhs._energyPoints;
+		this->_attackDamage = rhs._attackDamage;
+	}
+
+	return *this;
+}
---
+++ Module03/ex02/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <limits>
+#include "FragTrap.hpp"
+
+int	main( void ) {
+
+	FragTrap	defaultFragtrap;
+	FragTrap	bob( "Bob" );
+	FragTrap	bill( FragTrap( "Bill" ) );
+
+	std::cout << std::endl << "--------------------------------TURN 1--------------------------------" << std::endl;
+
+	defaultFragtrap.attack( "Training Dummy" );
+	bob.attack( "Training Dummy" );
+	bill.attack( "Training Dummy" );
+
+	std::cout << std::endl;
+
+	defaultFragtrap.takeDamage( 1 );
+	bob.takeDamage( 5 );
+	bill.takeDamage( std::numeric_limits<unsigned int>::max() );
+
+	std::cout << std::endl;
+
+	defaultFragtrap.beRepaired( std::numeric_limits<unsigned int>::max() );
+	bob.beRepaired( 0 );
+	bill.beRepaired( 10 );
+
+	std::cout << "----------------------------------------------------------------------" << std::endl;
+
+	std::cout << std::endl << "--------------------------------TURN 2--------------------------------" << std::endl;
+
+	defaultFragtrap.attack( "Training Dummy" );
+	bob.attack( "Training Dummy" );
+	bill.attack( "Training Dummy" );
+
+	std::cout << std::endl;
+
+	defaultFragtrap.takeDamage( std::numeric_limits<unsigned int>::max() );
+	bob.takeDamage( 4 );
+	bill.takeDamage( 5 );
+
+	std::cout << std::endl;
+
+	defaultFragtrap.beRepaired( std::numeric_limits<unsigned int>::max() );
+	bob.beRepaired( 3 );
+	bill.beRepaired( 10 );
+
+	std::cout << "----------------------------------------------------------------------" << std::endl;
+
+	std::cout << "--------------------------------TURN 3--------------------------------" << std::endl;
+
+	bob.attack( "Training Dummy" );
+	bob.beRepaired( 1 );
+
+	std::cout << "----------------------------------------------------------------------" << std::endl;
+
+	std::cout << std::endl << "--------------------------------TURN 4--------------------------------" << std::endl;
+
+	bob.attack( "Training Dummy" );
+	bob.beRepaired( 1 );
+
+	std::cout << "----------------------------------------------------------------------" << std::endl;
+
+	std::cout << std::endl << "--------------------------------TURN 5--------------------------------" << std::endl;
+
+	bob.attack( "Training Dummy" );
+	bob.beRepaired( 0 );
+
+	std::cout << "----------------------------------------------------------------------" << std::endl;
+
+	std::cout << std::endl << "--------------------------------TURN 6--------------------------------" << std::endl;
+
+	bob.attack( "Training Dummy" );
+	bob.beRepaired( 1 );
+
+	std::cout << "----------------------------------------------------------------------" << std::endl << std::endl;
+
+	return 0;
+}
